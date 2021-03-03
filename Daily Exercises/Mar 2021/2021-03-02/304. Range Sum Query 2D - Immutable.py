7cebd09239d5b3f2f04f003fa2ab82b122d58d3f
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-03 22:57:03 +0800

    Update 304. Range Sum Query 2D - Immutable.py

--- Daily Exercises/Mar 2021/2021-03-02/304. Range Sum Query 2D - Immutable.py
+++ Daily Exercises/Mar 2021/2021-03-02/304. Range Sum Query 2D - Immutable.py
@@ -1,12 +1,13 @@
 class NumMatrix:
-  def __init__(self, matrix):
-    nrow=len(matrix)
-    ncol=len(matrix[0]) if matrix else 0
-    self.sums=[[0]*(n+1) for _ in range(m+1)]  # Initialization
+  
+    def __init__(self, matrix):
+        nrow=len(matrix)
+        ncol=len(matrix[0]) if matrix else 0
+        self.sums=[[0]*(n+1) for _ in range(m+1)]  # Initialization
         
-    for i in range(m):
-      for j in range(n):
-        self.sums[i+1][j+1]=self.sums[i][j+1]+self.sums[i+1][j]-self.sums[i][j]+matrix[i][j]  # Compute the sum of all values from row 0->i and column 0->j
+        for i in range(m):
+            for j in range(n):
+                self.sums[i+1][j+1]=self.sums[i][j+1]+self.sums[i+1][j]-self.sums[i][j]+matrix[i][j]  # Compute the sum of all values from row 0->i and column 0->j
 
     def sumRegion(self, row1, col1, row2, col2):
-      return self.sums[row2+1][col+1]-self.sums[row1][col2+1]-self.sums[row2+1][col1]+self.sums[row1][col1]  # Compute the sum of values by the same method above
+        return self.sums[row2+1][col+1]-self.sums[row1][col2+1]-self.sums[row2+1][col1]+self.sums[row1][col1]  # Compute the sum of values by the same method above
