c353b6f3bb16b0075bb1db2ce2bc40aa2274d6e4
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-03 20:53:45 +0800

    Create 303 - Range Sum Query - Immutable.py

---
+++ Daily Exercises/Mar 2021/2021-03-01/303 - Range Sum Query - Immutable.py
@@ -0,0 +1,10 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.sums = [0]
+
+        for num in nums:
+            self.sums.append(self.sums[-1] + num)
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.sums[j + 1] - self.sums[i]
