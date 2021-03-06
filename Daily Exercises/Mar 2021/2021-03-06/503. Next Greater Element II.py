1be1a5fcd523965c4fb7c1c0f8d1dc8163613499
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-06 22:31:36 +0800

    Update 503. Next Greater Element II.py

--- Daily Exercises/Mar 2021/2021-03-06/503. Next Greater Element II.py
+++ Daily Exercises/Mar 2021/2021-03-06/503. Next Greater Element II.py
@@ -2,7 +2,7 @@ class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         n=len(nums)
         ans=[-1]*n
-        stack=list()  # monotonous stack (decreasing)
+        stack=list()  # monotonic stack (decreasing)
         for i in range(2*n-1):
             next_one=i%n
             while(len(stack) and nums[next_one]>nums[stack[-1]]):  # Pop until the new element satisfies monoticity
