666eb0e9017dff10d77732b5b6d17671d44aeee9
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-06 21:09:27 +0800

    Create 503. Next Greater Element II.py

---
+++ Daily Exercises/Mar 2021/2021-03-06/503. Next Greater Element II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        ans=[-1]*n
+        stack=list()  # monotonous stack (decreasing)
+        for i in range(2*n-1):
+            next_one=i%n
+            while(len(stack) and nums[next_one]>nums[stack[-1]]):  # Pop until the new element satisfies monoticity
+                ans[stack[-1]]=nums[next_one]
+                stack.pop()
+            if ans[next_one]==-1:  # A trick to reduce time complexity. In other words, marked element will not be marked again.
+                stack.append(next_one)
+        return ans
