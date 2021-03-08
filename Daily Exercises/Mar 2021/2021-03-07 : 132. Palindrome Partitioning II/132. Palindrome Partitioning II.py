c57e8e2021cdf919698efcf3c6bcdef194b8474f
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-08 14:20:00 +0800

    Create 132. Palindrome Partitioning II.py

---
+++ Daily Exercises/Mar 2021/2021-03-07 : 132. Palindrome Partitioning II/132. Palindrome Partitioning II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minCut(self, s: str) -> int:
+        n = len(s)
+        f = [[True] * n for _ in range(n)]  # f(i,j) stands for whether s[i:j+1] is a palindrome
+
+        for i in range(n - 1, -1, -1):  # note the correct the process order
+            for j in range(i + 1, n):
+                f[i][j] = (s[i] == s[j]) and f[i + 1][j - 1]  # DP
+
+        g=[n]*n
+
+        for i in range(n):
+            if f[0][i]:  # a palindrome needs no split
+                g[i]=0
+            else:
+                for j in range(i):
+                    if f[j+1][i]:  # transition formula: g[i] = min(g[j])+1, where 0<=j<=i-1
+                        g[i]=min(g[i],g[j]+1)
+        
+        return g[n-1]
