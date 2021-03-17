385b398f313ec7eb3dfc6d80c760362b4a5d88c3
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-17 09:53:45 +0800

    Update 115. Distinct Subsequences.py

--- Daily Exercises/Mar 2021/2021-03-17 : 115. Distinct Subsequences/115. Distinct Subsequences.py
+++ Daily Exercises/Mar 2021/2021-03-17 : 115. Distinct Subsequences/115. Distinct Subsequences.py
@@ -4,13 +4,13 @@ class Solution:
         if m < n:
             return 0
         
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp = [[0] * (n + 1) for _ in range(m + 1)]  # initialization
         for i in range(m + 1):
             dp[i][n] = 1
         
         for i in range(m - 1, -1, -1):
             for j in range(n - 1, -1, -1):
-                if s[i] == t[j]:
+                if s[i] == t[j]:  # transform formula
                     dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j]
                 else:
                     dp[i][j] = dp[i + 1][j]
