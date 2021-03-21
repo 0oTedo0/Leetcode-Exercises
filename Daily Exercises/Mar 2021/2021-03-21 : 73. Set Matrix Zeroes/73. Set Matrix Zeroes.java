f8a8cf2a4953309c48022718cb973ece35c6b0b1
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-21 10:19:53 +0800

    Create 73. Set Matrix Zeroes.java

---
+++ Daily Exercises/Mar 2021/2021-03-21 : 73. Set Matrix Zeroes/73. Set Matrix Zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean flag_col0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0]==0) {
+                flag_col0 = true;
+            }
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j]==0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0]==0 || matrix[0][j]==0) {
+                    matrix[i][j] = 0;
+                }
+            }
+            if (flag_col0) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
