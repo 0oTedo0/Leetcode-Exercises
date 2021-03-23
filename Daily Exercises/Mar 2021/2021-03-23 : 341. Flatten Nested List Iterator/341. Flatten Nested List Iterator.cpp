d60338d2580d59f061dd93933a9c65061cf1db65
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-23 11:07:51 +0800

    Create 341. Flatten Nested List Iterator.cpp

---
+++ Daily Exercises/Mar 2021/2021-03-23 : 341. Flatten Nested List Iterator/341. Flatten Nested List Iterator.cpp
@@ -0,0 +1,60 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+private:
+    // pair stores the current element，
+    // and the last element for checking whether iteration reaches the end
+    stack<pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator>> stk;
+
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        stk.emplace(nestedList.begin(), nestedList.end());
+    }
+
+    int next() {
+        // under the premise that every time 'next' is called,
+        // 'hasNext' will be called first,
+        // return the top element, and move the iterator
+        return stk.top().first++->getInteger();
+    }
+
+    bool hasNext() {
+        while (!stk.empty()) {
+            auto &p = stk.top();
+            if (p.first == p.second) { // the last element is found, pop it
+                stk.pop();
+                continue;
+            }
+            if (p.first->isInteger()) {
+                return true;
+            }
+            // if the current element is 'list', push it into stack
+            // and move the iterator pointer to it 
+            auto &lst = p.first++->getList();
+            stk.emplace(lst.begin(), lst.end());
+        }
+        return false;
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
