d4031c75a5a8bd061fe012dbe62aca450d4f8d61
Haoran Qian <61226180+0oTedo0@users.noreply.github.com>
2021-03-31 16:12:46 +0800

    Create 90. Subsets II.cpp

---
+++ Daily Exercises/Mar 2021/2021-03-01 : 90. Subsets II/90. Subsets II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        dfs(nums,0);
+        return ans;
+    }
+    void dfs(vector<int>& nums,int index){
+        if (index > nums.size()) return;
+        ans.push_back(path);
+        for (int i = index; i < nums.size(); ++i) {
+            if (i != index && nums[i] == nums[i - 1]) continue;
+            path.push_back(nums[i]);
+            dfs(nums,i+1);
+            path.pop_back();
+        }
+    }
+};
